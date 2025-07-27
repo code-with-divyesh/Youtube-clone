2b94643c212da17c91387373902650f5745d7d93
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-07-27 14:14:22 +0530

    Add Feed Components in home page

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import "./Feed.css";
+import { assets } from "../../assets";
+import { Link } from "react-router-dom";
+const Feed = () => {
+  return (
+    <div className="feed">
+      <Link to={`video/20/4251`} className="card">
+        <img src={assets.thumb1} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </Link>
+      <div className="card">
+        <img src={assets.thumb2} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb3} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb4} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb5} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb6} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb7} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb8} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb1} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb2} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb3} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb4} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb5} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb6} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb7} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={assets.thumb8} alt="" />
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi!
+        </h2>
+        <h3>code-with-divyesh</h3>
+        <p>15k views &bull;2 days ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Home.css";
 import SlideBar from "../../Components/SlideBar/SlideBar";
+import Feed from "../../Components/Feed/Feed";
 const Home = ({ sidebar }) => {
   return (
     <>
       <SlideBar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
